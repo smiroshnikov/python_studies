8565f4f733cd26446eae27d3bfa34457fa94e3b2
smiroshnikov <sergeim.qa@gmail.com>
2016-01-05 12:00:47 -0600

    Redoing flask tutorial

--- TTH-Flask/try02/template_intro.py
+++ TTH-Flask/try02/template_intro.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<name>")
 def index(name='Borbuth'):
-    return "Fuck You {}!".format(name)
+    return render_template("Borbuth.html")
 
 
 app.run(debug=True, port=8000, host='0.0.0.0')

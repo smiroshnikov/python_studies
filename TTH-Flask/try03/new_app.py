467841eb913f48a57af2dc6c98dc2444eb4995b1
smiroshnikov <sergeim.qa@gmail.com>
2016-01-06 09:20:38 -0600

    Bear Generator

---
+++ TTH-Flask/try03/new_app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    return "Saved!"
+
+
+app.run(debug=True, host='0.0.0.0', port=8000)

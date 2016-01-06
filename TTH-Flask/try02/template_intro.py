eccf2150e0da196a2d491f287d898ac80da10fb4
smiroshnikov <sergeim.qa@gmail.com>
2016-01-06 02:16:55 -0600

    Redoing flask tutorial , unpacked dictionary into Jinja template

--- TTH-Flask/try02/template_intro.py
+++ TTH-Flask/try02/template_intro.py
@@ -6,8 +6,14 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<user_name>")
-def index(user_name='Borbuth'):
-    return render_template("Borbuth.html", passed_variable=user_name)
+def index(user_name='Sergei Miroshnikov'):
+    return render_template("index.html", passed_variable=user_name)
+
+
+@app.route("/add/<int:A>/<int:B>")
+def add(A=2, B=2):
+    passed_dict = {'number1': A, 'number2': B}
+    return render_template("add.html", **passed_dict)  # unpacking dictionary
 
 
 app.run(debug=True, port=8000, host='0.0.0.0')

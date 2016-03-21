bae73d9186769dc4815495ced10ff8536b917e17
Iidwuurliik <sergeim.qa@gmail.com>
2016-03-21 15:56:26 +0200

    recalling

---
+++ GNS3/Python4NetworkEng/recall-strings.py
@@ -0,0 +1,12 @@
+device_type = "Cisco Switch"
+router_type = "Cisco"
+model_number = "29601"
+print device_type.count("c")
+print device_type.find("i")
+print device_type.find("xyz")
+print (device_type.upper() + "-> string remained immutable")
+print device_type.startswith("k")
+print device_type.endswith("h")
+
+# 3 important methods for string manupulation
+# split, strip, join

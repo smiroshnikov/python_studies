77c8acf061fdd75e836ed5fbfc5f5faf37f8ec08
Iidwuurliik <sergeim.qa@gmail.com>
2016-03-31 16:36:00 +0300

    persitent_routing Centos

--- GNS3/Python4NetworkEng/slicing-life-examples.py
+++ GNS3/Python4NetworkEng/slicing-life-examples.py
@@ -1,12 +1,11 @@
-import re  # is this regular expression
-
 traceroute = "80.179.166.85.static.012.net.il (80.179.166.85)  123.910 ms "
 OSPRF = "O 5.0.0.0/8 [110/128] via 6.0.0.2, 22:55:44, Serial0"
 
 second_ip = traceroute[traceroute.find("(") + 1: traceroute.find(")")]
-
-print traceroute.strip(".")  # ffs!
-print second_ip.split(".")[3]
+print ("Second IP is %s" % second_ip)
+print "*** + %s" % second_ip[:-10]
+print traceroute.strip(".")  # ffs! this is not working !
+print second_ip.split(".")[3]  # this will be useful
 """
 a = "80.179.166.85"
 print a.strip(".")

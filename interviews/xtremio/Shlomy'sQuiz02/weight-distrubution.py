25b19e3e9221bb5d8b7b8566dad09902a4b37f50
smiroshnikov <PerfectCareer1!>
2016-05-02 15:56:00 +0300

    shlomy quiz 2

--- interviews/xtremio/Shlomy'sQuiz02/weight-distrubution.py
+++ interviews/xtremio/Shlomy'sQuiz02/weight-distrubution.py
@@ -1,10 +1,10 @@
 test_set = [1, 2, 3, 4, 5, 6, 7, 8]
 
 
-def split_by_value(set):
-    right_sum = 0
-    left_sum = 0
-    for element in set: right_sum += set[element]
+def split_by_value(user_list):
+    right_part = user_list[len(test_set) / 2:len(test_set)]
+    left_part = user_list[0: len(test_set) / 2]
+    return left_part, right_part
 
 
-print (test_set[len(test_set) / 2:len(test_set)])
+print split_by_value(test_set)
